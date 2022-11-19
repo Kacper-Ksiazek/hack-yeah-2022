55d7739c5e85a078061fa43e3d8b2159feb88621
Jakub Wegrzyn <jakubek.weg@gmail.com>
2022-11-19 14:21:22 +0100

    Reinit firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyByhAul7u16FvJJwIbN_mF1h8A7BDmTrgs",
-    authDomain: "dont-waste.firebaseapp.com",
-    projectId: "dont-waste",
-    storageBucket: "dont-waste.appspot.com",
-    messagingSenderId: "153609106069",
-    appId: "1:153609106069:web:db4a754010c7fb22f8219c",
-    measurementId: "G-X89F3TPYXN",
+  apiKey: "AIzaSyByhAul7u16FvJJwIbN_mF1h8A7BDmTrgs",
+  authDomain: "dont-waste.firebaseapp.com",
+  projectId: "dont-waste",
+  storageBucket: "dont-waste.appspot.com",
+  messagingSenderId: "153609106069",
+  appId: "1:153609106069:web:db4a754010c7fb22f8219c",
+  measurementId: "G-X89F3TPYXN",
 };
 
 // Initialize Firebase
